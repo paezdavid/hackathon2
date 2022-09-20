85e15b6a4e89d496b987ef0d61fcfb60b529cf41
unknown <davidgithub1@gmail.com>
2022-09-20 18:45:17 -0400

    de a poco

--- index.py
+++ index.py
@@ -7,17 +7,24 @@ import requests
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
+
+   return render_template("landing.html")
+
+
+@app.route("/lista")
+def lista():
     # Columnas que voy a mostrar en el front
     datos_que_necesito = ["NOMBRE", "APELLIDO", "MODALIDAD", "LATITUD", "LONGITUD", "direcciones", "REG NRO"]
     # Dataframe donde se alojan las columnas extraídas
-    df = pd.read_excel("./testeo_markers.xlsx")
+    df = pd.read_excel("./plantillaa.xlsx")
     # Dataframe convertido a diccionario para mandarlo al frontend
     tabla_final = df.loc[:, datos_que_necesito].to_dict(orient='dict')
 
     
 
-    return render_template("index.html", tabla_final=tabla_final)
+    return render_template("lista.html", tabla_final=tabla_final)
+    
 
 
 
@@ -26,17 +33,33 @@ def hello_world():
 def perfil(perfil_id):
 
     # Leer el excel
-    df = pd.read_excel("./testeo_markers.xlsx")
+    df = pd.read_excel("./plantillaa.xlsx")
     
     # Buscar una fila con el valor del id y guardarla en una variable
     fila = df.loc[df["REG NRO"] == int(perfil_id)]
+
+    print(fila)
     
     # Convertir fila df a dict
-    fila_dict = fila.to_dict(orient='dict')
+    fila_dict = fila.to_dict(orient='list')
+
+    print(fila_dict)
 
     return render_template("perfil.html", fila=fila_dict)
+    # return render_template("landing.html")
+    # return render_template("filtros.html")
 
     
 
     # session['my_var'] = 'my_value'
     # return redirect(url_for('b'))
+
+@app.route("/perfiless", methods = ['POST'])
+def perfiil():
+
+    check = request.form.to_dict()
+    print(check)
+
+    return "hola"
+
+
